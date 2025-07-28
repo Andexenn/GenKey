ae1e206975affa742fbf4c1489f02337a9e7acd1
ryan <phanhuynhhung0707@gmail.com>
2025-07-28 16:55:41 +0700

    4. second page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "HUNG"
 
 @app.route('/')
 def home():
@@ -8,7 +11,16 @@ def home():
 
 @app.route('/gen', methods=['GET', 'POST'])
 def gen():
-    
+    # TODO: consider the methods get or post, create the pass
+    if request.method == "POST":
+        user_name = request.form["username"]
+        if not user_name:
+            flash("You've not entered user name")
+            return redirect(url_for('gen'))
+        else:
+            Pass="123"
+            return render_template('gen.html', Pass="123")
+            # flash("Your name has been used", "info")
     return render_template('gen.html')
 
 if __name__ == "__main__":
--- static/main.js
+++ static/main.js
@@ -1,5 +1,5 @@
 AOS.init({
-  duration: 1000, // Thời gian hiệu ứng (tính bằng ms)
-  easing: "ease-in-out", // Loại hiệu ứng easing
-  once: true, // Nếu true, hiệu ứng chỉ chạy 1 lần khi cuộn đến phần tử
+  duration: 1000, 
+  easing: "ease-in-out", 
+  once: true, 
 });
